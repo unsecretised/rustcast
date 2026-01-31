23d31b8c334f0dc7c9fcb58e49a3029c7df5a3f3
Mnem42 <Mnem424242@gmail.com>
2026-01-31 18:23:19 +0000

    feat: bigger taskbar icon size

--- src/app.rs
+++ src/app.rs
@@ -80,7 +80,7 @@ pub fn default_settings() -> Settings {
             width: WINDOW_WIDTH,
             height: DEFAULT_WINDOW_HEIGHT,
         },
-        icon: Some(crate::icon::iced_icon::icon_64()),
+        icon: Some(crate::icon::iced_icon::icon_128()),
         ..Default::default()
     }
 }
